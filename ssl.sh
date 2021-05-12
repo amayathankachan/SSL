4520a0bad90508e09b88a7e7cc18d8d596c69ff6
amayathankachan <84068178+amayathankachan@users.noreply.github.com>
2021-05-12 04:50:52 -0700

    update

--- ssl.sh
+++ ssl.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# Purpose: Alert sysadmin/developer about the TLS/SSL cert expiry date in advance
+# Alert once SSL certificate expires
 # -------------------------------------------------------------------------------
 DOM="teistafoods.com"
 PORT="443"
 
-# 7 days in seconds 
-DAYS="604800" 
- 
-# Email settings 
-_sub="$DOM will expire within $DAYS (7 days)."
+
+DAYS=`date -d "+30 days"`
+
+# Email settings
+_sub="SSL certificate of $DOM will expire within $DAYS "
 _from="amayaparunthaniyil555@gmail.com"
 _to="amaya_thankachan@yahoo.com"
-#openssl="/usr/local/src/"
-openssl s_client -servername $DOM -connect $DOM:$PORT | openssl x509 -enddate -noout  
- 
-# Send email and push message to my mobile
-if [ $? -eq 0 ]
+Certexp=`openssl s_client -servername $DOM -connect $DOM:$PORT | openssl x509 -enddate -noout`
+
+# Send email
+if [ "$Certexp" = "$DAYS" ]
+#if [ $? -eq 0 ]
 then
-	echo "${_sub}"
-        mail -s "$_sub" -r "$_from" "$_to" <<< "SSL certificate ($DOM) will expire soon on [$(date)]"
-fi        
+echo "${_sub}"
+      mail -s "$_sub" -r "$_from" "$_to" <<< "SSL certificate ($DOM) will expire soon on[$(date)]"
+fi
